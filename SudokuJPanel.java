d0c7effa40169ef2999565c7d9b097ac9aee7da4
nir nicole <54746985+nirnicole@users.noreply.github.com>
2020-01-26 02:03:17 +0200

    Update SudokuJPanel.java

--- SudokuJPanel.java
+++ SudokuJPanel.java
@@ -22,57 +22,32 @@ public class SudokuJPanel extends JPanel {
 	}
 	
 	//paint
-    public void paintComponent(Graphics g)
-    {
+	public void paintComponent(Graphics g)
+	{
 	   super.paintComponent(g); 
-	   
+
 	   Font font = new Font("Verdana", Font.BOLD, 16);
 	   g.setFont(font);
-	  
+
 	   if(this.locked)
 			  g.setColor(Color.BLUE);
-	   
+
 	   if(this.number>=1 && this.number<=9)
 		   g.drawString(Integer.toString(this.number), getWidth()/2, getHeight()/2);
-	   
-	   
-    }
-	
-    //getters and setters
-    //
-	public int getLineIndex() {
-		return lineIndex;
-	}
-	
-	public int getColumnIndex() {
-		return columnIndex;
-	}
-    
-	public int getNumber() {
-		return number;
-	}
-	
-	public void setNumber(int n) {
-		number = n; 
-	}
-	
-	public boolean isLocked() {
-		return locked;
-	}
-	
-	public void setLocked(boolean flag) {
-		locked = flag;
-	}
-	
 
-	public Color getColor() {
-		return this.beckgroundColor;
+
 	}
 	
-	public void setColor(Color c) {
-		this.beckgroundColor = c;
-	}
-	
+    	//getters and setters
+  	//
+	public int getLineIndex() {return lineIndex;}
+	public int getColumnIndex() {return columnIndex;}
+	public int getNumber() {return number;}
+	public void setNumber(int n) {number = n;}
+	public boolean isLocked() {return locked;}
+	public void setLocked(boolean flag) {locked = flag;}
+	public Color getColor() {return this.beckgroundColor;}
+	public void setColor(Color c) {this.beckgroundColor = c;}
 	
 	//clean panel
 	public void clearPanel() {
@@ -80,4 +55,4 @@ public class SudokuJPanel extends JPanel {
 		locked = false;
 	}
 	
-}
+}//end of class
